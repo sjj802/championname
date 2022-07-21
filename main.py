c1727bf8435240e31d107512f9823120c2a5c932
sjj802 <85326315+sjj802@users.noreply.github.com>
2022-07-21 10:52:40 +0900

    championname 아무무를 추가했음

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 
 url='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/Aatrox.json'
 url3='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/Amumu.json'
+
+res=requests.get(url).json()
 res=requests.get(url).json()
 
 champ_lst=(res['data']['Aatrox']['key'], res['data']['Aatrox']['name'])
+champ_lst2=(res2['data']['Amumu']['key'], res2['data']['Amumu']['name'])
