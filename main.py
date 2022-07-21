a4d8dc36ffba18d93b8615a4c7370c23ee597b34
sjj <sjj802@naver.com>
2022-07-21 10:55:02 +0900

    champion Ahri 등록

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 
 url='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/Aatrox.json'
+url2='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/Ahri.json'
 url3='http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/Amumu.json'
 
 res=requests.get(url).json()
-res=requests.get(url).json()
+res2=requests.get(url3).json()
+res3 = requests.get(url2).json()
 
 champ_lst=(res['data']['Aatrox']['key'], res['data']['Aatrox']['name'])
 champ_lst2=(res2['data']['Amumu']['key'], res2['data']['Amumu']['name'])
+champ_lst3=(res3['data']['Ahri']['key'], res2['data']['Ahri']['name'])
